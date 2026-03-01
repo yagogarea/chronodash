67c251ed8af43944830d3cb78b888e2e1c8bb213
paulacarril <145045483+paulacarril@users.noreply.github.com>
2026-03-01 04:47:27 +0100

    feat: add GPLv3 licensing information to all relevant files

--- lib/chronodash/alerting/providers/discord.ex
+++ lib/chronodash/alerting/providers/discord.ex
@@ -1,3 +1,20 @@
+# Elixir backend that analyzes meteoFIX data, provides insights, and integrates with Grafana.
+
+# Copyright (C) 2026 Santiago Garea Cidre, Paula Carril Gontán, and Saúl Zas Carballal
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see [GNU GPLv3](https://www.gnu.org/licenses/gpl-3.0.en.html).
+
 defmodule Chronodash.Alerting.Providers.Discord do
   @moduledoc """
   Discord implementation for alert delivery.
